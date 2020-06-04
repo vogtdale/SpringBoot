1beb3cabfa2ce367df1d920c9bfe9179c67e89ce
Xenomorph2 <charlesdickens21@gmail.com>
2020-06-04 16:34:10 +0200

    fix id user

--- src/main/java/com/meetup/Model/User.java
+++ src/main/java/com/meetup/Model/User.java
@@ -21,7 +21,7 @@ public class User {
 
     @Column(name = "ID")
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @NotBlank
